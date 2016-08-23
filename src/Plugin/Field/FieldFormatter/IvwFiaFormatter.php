c5700ba79ccf5ee0b61b8c32ede8de636f9bf602
Patrick Durold <patrick.durold@valiton.com>
2016-08-23 22:40:19 +0200

    - reformatting / coding style

--- src/Plugin/Field/FieldFormatter/IvwFiaFormatter.php
+++ src/Plugin/Field/FieldFormatter/IvwFiaFormatter.php
@@ -10,14 +10,14 @@ namespace Drupal\ivw_integration_fia\Plugin\Field\FieldFormatter;
 use Drupal\Component\Render\HtmlEscapedText;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Field\FieldDefinitionInterface;
-use Drupal\Core\Field\FormatterBase;
 use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Render\BubbleableMetadata;
 use Drupal\Core\Utility\Token;
 use Drupal\ivw_integration\IvwLookupServiceInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use ReflectionClass;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Plugin implementation of the 'ivw_fia_formatter' formatter.
@@ -69,9 +69,27 @@ class IvwFiaFormatter extends FormatterBase implements ContainerFactoryPluginInt
    * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
    * @param \Drupal\Core\Utility\Token $token
    */
-  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings,
-    IvwLookupServiceInterface $ivwLookupService, ConfigFactoryInterface $configFactory, Token $token) {
-    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
+  public function __construct(
+    $plugin_id,
+    $plugin_definition,
+    FieldDefinitionInterface $field_definition,
+    array $settings,
+    $label,
+    $view_mode,
+    array $third_party_settings,
+    IvwLookupServiceInterface $ivwLookupService,
+    ConfigFactoryInterface $configFactory,
+    Token $token
+  ) {
+    parent::__construct(
+      $plugin_id,
+      $plugin_definition,
+      $field_definition,
+      $settings,
+      $label,
+      $view_mode,
+      $third_party_settings
+    );
     $this->ivwLookupService = $ivwLookupService;
     $this->configFactory = $configFactory;
     $this->tokenService = $token;
@@ -80,41 +98,12 @@ class IvwFiaFormatter extends FormatterBase implements ContainerFactoryPluginInt
   /**
    * {@inheritdoc}
    */
-  public function viewElements(FieldItemListInterface $items, $langcode = NULL) {
-    $elements = array();
-    $fiaDomainPrefix = $this->configFactory->get('ivw_integration_fia.settings')->get('fia_domain_prefix');
-
-    // core token devs don't like the concept of callable
-    // getting the method of the static class as closure keeps the callback overridable by subclasses
-    $class = new ReflectionClass(static::class);
-    $callback = $class->getMethod('alterReplacements')->getClosure();
-
-    foreach ($items as $delta => $item) {
-      $url = $fiaDomainPrefix . '?' . http_build_query(array(
-            'st' => $this->configFactory->get('ivw_integration.settings')->get('mobile_site'), // maybe use mew value here?
-            'cp' => $this->tokenService ->replace(
-              $this->configFactory->get('ivw_integration.settings')->get('code_template'),
-              ['entity' => $items->getEntity()],
-              [
-                'sanitize' => FALSE,
-                'callback' => $callback,
-              ]
-            )
-          )
-        );
-
-      $elements[$delta] = array(
-        '#theme' => 'ivw_fia',
-        '#fia_frame_url' => $url,
-      );
-    }
-    return $elements;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+  public static function create(
+    ContainerInterface $container,
+    array $configuration,
+    $plugin_id,
+    $plugin_definition
+  ) {
     return new static(
       $plugin_id,
       $plugin_definition,
@@ -141,11 +130,59 @@ class IvwFiaFormatter extends FormatterBase implements ContainerFactoryPluginInt
    * @param array $options
    * @param \Drupal\Core\Render\BubbleableMetadata|NULL $bubbleable_metadata
    */
-  public static function alterReplacements(array &$replacements, array $data = array(), array $options = array(), BubbleableMetadata $bubbleable_metadata = NULL) {
+  public static function alterReplacements(
+    array &$replacements,
+    array $data = [],
+    array $options = [],
+    BubbleableMetadata $bubbleable_metadata = NULL
+  ) {
     if (isset($replacements['[ivw:delivery]'])) {
       // Token calls the callback after escaping the replacements...
       $replacements['[ivw:delivery]'] = new HtmlEscapedText('2');
     }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(
+    FieldItemListInterface $items,
+    $langcode = NULL
+  ) {
+    $elements = [];
+    $fiaDomainPrefix = $this->configFactory->get('ivw_integration_fia.settings')
+      ->get('fia_domain_prefix');
+
+    // core token devs don't like the concept of callable
+    // getting the method of the static class as closure keeps the callback overridable by subclasses
+    $class = new ReflectionClass(static::class);
+    $callback = $class->getMethod('alterReplacements')->getClosure();
+
+    foreach ($items as $delta => $item) {
+      $url = $fiaDomainPrefix . '?' . http_build_query(
+          array(
+            'st' => $this->configFactory->get('ivw_integration.settings')->get(
+              'mobile_site'
+            ), // maybe use mew value here?
+            'cp' => $this->tokenService->replace(
+              $this->configFactory->get('ivw_integration.settings')->get(
+                'code_template'
+              ),
+              ['entity' => $items->getEntity()],
+              [
+                'sanitize' => FALSE,
+                'callback' => $callback,
+              ]
+            ),
+          )
+        );
+
+      $elements[$delta] = [
+        '#theme' => 'ivw_fia',
+        '#fia_frame_url' => $url,
+      ];
+    }
+    return $elements;
   }
 
 
